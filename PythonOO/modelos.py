6163d88e4d8e7f642a7dff1a373a78932efc66b7
Eduardo Palmerim <106132039+allefzy@users.noreply.github.com>
2025-02-25 11:30:02 -0300

    Models.py

--- PythonOO/modelos.py
+++ PythonOO/modelos.py
@@ -1,74 +1,74 @@
-class Programa:
-    def __init__(self, nome, ano):
-        self._nome = nome.title()
-        self.ano = ano
+class Program:
+    def __init__(self, name, year):
+        self._name = name.title()
+        self.year = year
         self._likes = 0
 
     @property
     def likes(self):
         return self._likes
 
-    def dar_likes(self):
+    def give_like(self):
         self._likes += 1
 
     @property
-    def nome(self):
-        return self._nome
+    def name(self):
+        return self._name
 
-    @nome.setter
-    def nome(self, nome):
-        self._nome = nome
+    @name.setter
+    def name(self, name):
+        self._name = name
 
     def __str__(self):
-        return f'Nome: {self.nome} Likes: {self.likes}'
+        return f'Name: {self.name} Likes: {self.likes}'
 
-class Filme(Programa):
-    def __init__(self, nome, ano, duracao):
-        super().__init__(nome, ano)
-        self.duracao = duracao
+class Movie(Program):
+    def __init__(self, name, year, duration):
+        super().__init__(name, year)
+        self.duration = duration
     
     def __str__(self):
-        return f'Nome: {self.nome} - {self.duracao} min - Likes: {self.likes}'
+        return f'Name: {self.name} - {self.duration} min - Likes: {self.likes}'
 
-class Serie(Programa):
-    def __init__(self, nome, ano, temporadas):
-        super().__init__(nome, ano)
-        self.temporadas = temporadas
+class Series(Program):
+    def __init__(self, name, year, seasons):
+        super().__init__(name, year)
+        self.seasons = seasons
 
     def __str__(self):
-        return f'Nome: {self.nome} - {self.temporadas} temporadas - Likes: {self.likes}'
+        return f'Name: {self.name} - {self.seasons} seasons - Likes: {self.likes}'
 
 
-class Playlist():
-    def __init__(self, nome, programas):
-        self.nome = nome
-        self._programas = programas
+class Playlist:
+    def __init__(self, name, programs):
+        self.name = name
+        self._programs = programs
 
-    def __getitem__(self, item)
-        return self._programas[item]
+    def __getitem__(self, item):
+        return self._programs[item]
 
     def __len__(self):
-        return len(self._programas)
+        return len(self._programs)
 
-vingadores = Filme('vingadores - guerra infinita', 2018, 160)
-atlanta = Serie('atlanta', 2018, 2)
-tmep = Filme('todo mundo em panico', 1999, 100)
-demolidor = Serie('demolidor', 2016, 2)
+avengers = Movie('avengers - infinity war', 2018, 160)
+atlanta = Series('atlanta', 2018, 2)
+scary_movie = Movie('scary movie', 1999, 100)
+daredevil = Series('daredevil', 2016, 2)
 
-vingadores.dar_likes()
-vingadores.dar_likes()
-vingadores.dar_likes()
-atlanta.dar_likes()
-atlanta.dar_likes()
-tmep.dar_likes()
-tmep.dar_likes()
-demolidor.dar_likes()
-demolidor.dar_likes()
+avengers.give_like()
+avengers.give_like()
+avengers.give_like()
+atlanta.give_like()
+atlanta.give_like()
+scary_movie.give_like()
+scary_movie.give_like()
+daredevil.give_like()
+daredevil.give_like()
 
-listinha = [atlanta, vingadores, demolidor, tmep]
-minha_playlist = Playlist('fim de semana', listinha)
+playlist_list = [atlanta, avengers, daredevil, scary_movie]
+my_playlist = Playlist('weekend', playlist_list)
 
-for programa in minha_playlist:
-    print(programa)
+for program in my_playlist:
+    print(program)
 
-print(f'Tamanho: {len(minha_playlist.listagem)}')
+print(f'Size: {len(my_playlist)}')
